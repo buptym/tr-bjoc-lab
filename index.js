2c391adb6e712ba32225cef42185edc9cf2452e6
Ming.Yang <buptym@vip.qq.com>
2017-10-01 23:55:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,7 @@ function q_quick_response(req,res) {
                                 "text": result.rows[0].thread1,
                                 "type": "button",
                                 "value": result.rows[0].thread1,
+				"fallback": result.rows[0].title,
                                 "callback_id": result.rows[0].title
                             }]
                         }
