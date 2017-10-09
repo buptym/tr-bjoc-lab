4ef1995e9bcb9191f785b424082e064ef71c7153
Ming.Yang <buptym@vip.qq.com>
2017-10-09 12:05:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 const app = express();
